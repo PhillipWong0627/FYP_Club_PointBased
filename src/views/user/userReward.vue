89b7d38c9c85c9abcbeeea07c2983c041a35d05c
PhillipWong0627 <85791085+PhillipWong0627@users.noreply.github.com>
2024-08-05 21:57:28 +0800

    QR Code Page

--- src/views/user/userReward.vue
+++ src/views/user/userReward.vue
@@ -2,10 +2,29 @@
     <div>
         <Navnavbars></Navnavbars>
         <UserNavbar></UserNavbar>
-        Rewards
 
-        <div style="width: 500px; height: 500px;" class="border-2 border-red-500">
-            QR CODE
+        <p class="flex justify-center text-3xl uppercase mt-6">Rewards</p>
+
+
+        <div style=" height: 500px;" class="border-2 border-red-500 flex justify-center">
+            <div class="border-2 border-red-500 bg-white flex justify-center" style="width: 500px;">
+                <div class="flex flex-col bg-emerald-200">
+                    <div>
+                        RIGHT ICON
+                    </div>
+                    <div class="border-2 border-red-500 bg-white flex flex-col items-center py-2" style="width: 250px;">
+                        <qrcode-vue :value="value" :size="size" level="H" />
+                        <span class="flex justify-center text-sm">M 259 99</span>
+                    </div>
+                    <div>
+                        WORDS
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </div>
 
         <div class="flex flex-col">
@@ -16,7 +35,7 @@
             <div>
                 Cards..........
             </div>
-            
+
         </div>
         <div class="flex flex-col">
             <span>Deals</span>
@@ -26,7 +45,7 @@
             <div>
                 Cards..........
             </div>
-            
+
         </div>
 
         <FooterComp class="mt-12"></FooterComp>
@@ -38,18 +57,26 @@ import Navnavbars from "@/components/Navnavbars/navNavBar.vue";
 import FooterComp from "@/components/Footers/Footer.vue";
 import UserNavbar from '@/components/Navbars/UserNavbar.vue'
 
+//QR Code
+import QrcodeVue from 'qrcode.vue'
+
 export default {
     components: {
         Navnavbars,
         FooterComp,
         UserNavbar,
+
+        //QR Code
+        QrcodeVue
     },
     methods: {
 
     },
     data() {
         return {
-    
+            value: 'https://www.linkedin.com/in/wong-liang-jian-602176230/',
+            size: 225,
+
 
         }
     }
@@ -57,4 +84,3 @@ export default {
 
 
 </script>
-
