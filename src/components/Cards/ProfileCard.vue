1e16b01a71ed7010d1b7d7ff85f8a384f1beb459
PhillipWong0627 <85791085+PhillipWong0627@users.noreply.github.com>
2024-09-08 17:50:46 +0800

    Profile Picture
    OTW TO FACILITY !!!!!!!!!!!!!!!!!

--- src/components/Cards/ProfileCard.vue
+++ src/components/Cards/ProfileCard.vue
@@ -1,124 +1,149 @@
 <template>
-    <div
-      class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0"
-    >
-      <div class="rounded-t bg-white mb-0 px-6 py-6">
-        <div class="text-center flex justify-between">
-          <h6 class="text-blueGray-700 text-xl font-bold">My account</h6>
-          <button
-            class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-            type="button"
-          >
-            Edit
-          </button>
+  <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+    <div class="flex flex-wrap justify-center">
+      <div class="w-full px-4 flex justify-center ">
+        <div class="relative ">
+          <!-- <img alt="..." :src="team2"
+            class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px" /> -->
+          <img :src="team2" class="shadow-xl rounded-full h-auto align-middle border-none "
+            style="width: 140px; height: 140px; object-fit: cover;" />
+          <!-- Add upload button here -->
+          <label for="upload" class="absolute bottom-0 right-0 cursor-pointer">
+            <img src="@/assets/upload-icon.png" style="width: 24px; height: 24px;" />
+            <input type="file" id="upload" style="display: none;" @change="handleImageUpload" />
+          </label>
+
         </div>
       </div>
-      <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-        <form>
-          <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            User Information
-          </h6>
-          <div class="flex flex-wrap">
-            <div class="w-full lg:w-6/12 px-4">
-              <div class="relative w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  htmlFor="grid-password"
-                >
-                  Username
-                </label>
-                <input
-                  type="text"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  value="lucky.jesse"
-                />
-              </div>
-            </div>
-            <div class="w-full lg:w-6/12 px-4">
-              <div class="relative w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  htmlFor="grid-password"
-                >
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  value="jesse@example.com"
-                />
-              </div>
-            </div>
 
-          </div>
-  
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
-  
-          <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            Contact Information
-          </h6>
-          <div class="flex flex-wrap">
-            <div class="w-full lg:w-12/12 px-4">
-              <div class="relative w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  htmlFor="grid-password"
-                >
-                  Address
-                </label>
-                <input
-                  type="text"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                />
-              </div>
-            </div>
-            <div class="w-full lg:w-4/12 px-4">
-              <div class="relative w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  htmlFor="grid-password"
-                >
-                  Contact Number
-                </label>
-                <input
-                  type="email"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  value="New York"
-                />
-              </div>
-            </div>
+    </div>
 
-          </div>
-  
-          <hr class="mt-6 border-b-1 border-blueGray-300" />
-  
-          <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            About Me
-          </h6>
-          <div class="flex flex-wrap">
-            <div class="w-full lg:w-12/12 px-4">
-              <div class="relative w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  htmlFor="grid-password"
-                >
-                  About me
-                </label>
-                <textarea
-                  type="text"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  rows="4"
-                >
-                      A beautiful UI Kit and Admin for VueJS & Tailwind CSS. It is Free
-                      and Open Source.
-                    </textarea
-                >
-              </div>
-            </div>
-          </div>
-        </form>
+    <div class="rounded-t bg-white mb-0 px-6 py-6">
+      <div class="text-center flex justify-between">
+        <h6 class="text-blueGray-700 text-xl font-bold">My account</h6>
+        <button
+          class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+          type="button">
+          Edit
+        </button>
       </div>
     </div>
-  </template>
-  
+    <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+      <form>
+        <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+          User Information
+        </h6>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-6/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                Username
+              </label>
+              <input type="text"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                value="lucky.jesse" />
+            </div>
+          </div>
+          <div class="w-full lg:w-6/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                Email address
+              </label>
+              <input type="email"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                value="jesse@example.com" />
+            </div>
+          </div>
+
+        </div>
+
+        <hr class="mt-6 border-b-1 border-blueGray-300" />
+
+        <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+          Contact Information
+        </h6>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-12/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                Address
+              </label>
+              <input type="text"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" />
+            </div>
+          </div>
+          <div class="w-full lg:w-4/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                Contact Number
+              </label>
+              <input type="email"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                value="New York" />
+            </div>
+          </div>
+
+        </div>
+
+        <hr class="mt-6 border-b-1 border-blueGray-300" />
+
+        <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+          About Me
+        </h6>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-12/12 px-4">
+            <div class="relative w-full mb-3">
+              <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                About me
+              </label>
+              <textarea type="text"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                rows="4">
+                      A beautiful UI Kit and Admin for VueJS & Tailwind CSS. It is Free
+                      and Open Source.
+                    </textarea>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import team2 from "@/assets/img/team-2-800x800.jpg";
+
+export default {
+  data() {
+    return {
+      team2,
+    };
+  },
+  methods: {
+    // async handleImageUpload(event) {
+    //   // Access the selected file
+    //   const selectedFile = event.target.files[0];
+
+    //   // send OTP to user phone
+    //   const result = await updateProfilePic(selectedFile);
+
+    //   if (result === true) {
+    //     // show modal
+    //     // this.gobackmypage()
+
+    //     // Set a flag in local storage to show the edit profile modal after refreshing
+    //     localStorage.setItem('gobackmypage', 'true');
+    //     window.location.reload();
+
+    //   } else if (result === false) {
+    //     this.warningMessage = "Please check your network";
+    //   } else {
+    //     this.warningMessage = result;
+    //   }
+    // },
+  }
+};
+
+
+</script>
