9fe07b7e4df778d6f1a053373eb0c9e2ec63ea36
PhillipWong0627 <85791085+PhillipWong0627@users.noreply.github.com>
2024-09-24 01:13:02 +0800

    Update apiProvider.js

--- src/service/apiProvider.js
+++ src/service/apiProvider.js
@@ -3,7 +3,7 @@ import {
   getRequest,
   patchRequest,
   deleteRequest,
-  // postFileRequest
+  postFileRequest
 } from '@/service/apiRequestMethod';
 
 import {
@@ -17,6 +17,7 @@ import {
   makePayment,
   getAllFacility,
   getAllEvents,
+  cloudinaryUrl
 
 } from '@/utils/apiConfig.js';
 import { postRequest } from './apiRequestMethod';
@@ -289,3 +290,33 @@ export async function getEvents() {
   }
 
 }
+
+// Update Image
+export async function uploadImage(file, resourcetype) {
+  const cloudName = "135839877197964"
+
+  const url = cloudinaryUrl
+    .replace("{cloud name}", cloudName)
+    .replace("{resource_type}", resourcetype);
+
+  console.log(url)
+  try {
+    const response = await postFileRequest(file, url);
+    const status = response.status;
+
+    // const data = response.data;
+    // console.log(response);
+    // console.log(response.data.url);
+
+    if (status === 200) {
+      return response;
+    } else {
+      console.log(`Unsuccessfully upload to Cloudinary: ${response.status}`);
+      return false;
+    }
+  } catch (e) {
+    console.log(`Unsuccessful upload to Cloudinary: ${e}`);
+    return false;
+  }
+}
+
